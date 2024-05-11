e32b34683b710e13e624339efe9a0afa620d883e
ValenTiN Konov <vsrknv@gmail.com>
2024-05-11 12:18:06 +0300

    Func learn

---
+++ ton-course/test-contract/tests/main.spec.ts
@@ -0,0 +1,39 @@
+import { Cell, toNano } from "@ton/core";
+import { hex } from "../build/main.compiled.json";
+import { Blockchain } from "@ton/sandbox";
+import { MainContract } from "../wrappers/MainContract";
+import "@ton/test-utils";
+
+describe("main.fc contract tests", () => {
+    it("should successfully increase counter in contract and get the proper most recent sender address", async () => {
+        const blockchain = await Blockchain.create();
+        const codeCell = Cell.fromBoc(Buffer.from(hex, "hex"))[0];
+        const initAddress = await blockchain.treasury("initAddress");
+
+        const myContract = blockchain.openContract(
+            await MainContract.createFromConfig({
+                number: 0,
+                address: initAddress.address
+            }, codeCell)
+        );
+
+        const senderWallet = await blockchain.treasury("sender");
+
+        const sentMessageResult = await myContract.sendIncrement(
+            senderWallet.getSender(),
+            toNano("0.05"),
+            5
+        );
+
+        expect(sentMessageResult.transactions).toHaveTransaction({
+            from: senderWallet.address,
+            to: myContract.address,
+            success: true,
+        });
+
+        const data = await myContract.getData();
+
+        expect(data.recent_sender.toString()).toBe(senderWallet.address.toString());
+        expect(data.number).toEqual(5);
+    });
+});
